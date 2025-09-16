7fdc9b933eb2979ffa03525bbd71f7bb29f582fd
William Kurzius <bill@kurzius.net>
2025-09-16 18:38:54 -0400

    i have no idea

--- helloWorld.java
+++ helloWorld.java
@@ -1,7 +1,23 @@
+/*
+This program will output the text "Hello, world!",
+as is customary when learning a new language.
+*/
 public class helloWorld {
+    public static void main(String[] args) {
 
-    public static void main (String[] args) {
-    
-        System.out.println("Hello, world!");
+        int x = 100;
+
+        x -= 5;
+        System.out.println(x);
+
+        x *= 5;
+        System.out.println(x);
+
+        x /= 5;
+        System.out.println(x);
+
+        x %= 5;
+        System.out.println(x);
+
     }
 }
