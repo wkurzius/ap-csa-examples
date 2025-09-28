9cb4933ed8a6b04809fc74c14c72ce8889537f5a
William Kurzius <bill@kurzius.net>
2025-09-28 17:08:30 -0400

    random loop

--- helloWorld.java
+++ helloWorld.java
@@ -1,22 +1,11 @@
-/**
- * The HelloWorld program implements an application that
- * simply displays "Hello World!" to the standard output.
- * 
- * @author Hackerman
- * @version 1.0
- */
+
 public class helloWorld {
 
-    // The main method, obviously.
     public static void main(String[] args) {
 
-        /*
-         * The code below will print the words Hello World
-         * to the screen, and it is amazing
-         */
-        System.out.println("Hello, world!");
-
-        /* Turns out this is fine, too. */
+        for (int i=0; i<100; i++) {
+            System.out.println(Math.random()*10 + 10);
+        }
 
     }
 }
