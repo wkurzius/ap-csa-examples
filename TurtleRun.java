926fd1ec1af3a28a1b1d4c09d2d165a8a62c5046
William Kurzius <bill@kurzius.net>
2025-10-02 12:26:20 -0400

    stuff

--- TurtleRun.java
+++ TurtleRun.java
@@ -1,17 +1,59 @@
+import java.util.concurrent.TimeUnit;
 import JavaSwingTurtle.*;
-import java.awt.*;
-import java.util.*;
 
 public class TurtleRun {
     public static void main(String[] args) {
-        // Do not change the next 2 lines
-        World habitat = new World(300, 300);
-        Turtle yertle = new Turtle(habitat);
 
-        // Code to make yertle move forward and turn right
+        World habitat = new World(300, 300);
+        World habitat2 = new World();
+
+        Turtle yertle = new Turtle(habitat);
+        Turtle yertle2 = new Turtle(20, 20,habitat);
+
+
+         habitat.show(true);
+
+        yertle.forward();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        yertle.turnRight();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        yertle.forward();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        yertle.turnRight();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
         yertle.forward();
         yertle.turnRight();
 
-        habitat.setVisible(true);
+        System.out.println(getVersion());
+
+    }
+
+    private static int getVersion() {
+        String version = System.getProperty("java.version");
+        if (version.startsWith("1.")) {
+            version = version.substring(2, 3);
+        } else {
+            int dot = version.indexOf(".");
+            if (dot != -1) {
+                version = version.substring(0, dot);
+            }
+        }
+        return Integer.parseInt(version);
     }
 }
