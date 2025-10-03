625eab902968d246b77ef8ade1c18565e45e14d6
William Kurzius <bill@kurzius.net>
2025-10-03 09:09:53 -0400

    blah

--- TurtleRun.java
+++ TurtleRun.java
@@ -8,52 +8,10 @@ public class TurtleRun {
         World habitat2 = new World();
 
         Turtle yertle = new Turtle(habitat);
-        Turtle yertle2 = new Turtle(20, 20,habitat);
+        Turtle yertle2 = new Turtle(20, 20, habitat);
 
-
-         habitat.show(true);
+        habitat.show(true);
 
         yertle.forward();
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
-        yertle.turnRight();
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
-        yertle.forward();
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
-        yertle.turnRight();
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
-        yertle.forward();
-        yertle.turnRight();
-
-        System.out.println(getVersion());
-
-    }
-
-    private static int getVersion() {
-        String version = System.getProperty("java.version");
-        if (version.startsWith("1.")) {
-            version = version.substring(2, 3);
-        } else {
-            int dot = version.indexOf(".");
-            if (dot != -1) {
-                version = version.substring(0, dot);
-            }
-        }
-        return Integer.parseInt(version);
     }
 }
--- helloWorld.java
+++ helloWorld.java
@@ -3,9 +3,7 @@ public class helloWorld {
 
     public static void main(String[] args) {
 
-        for (int i=0; i<100; i++) {
-            System.out.println(Math.random()*10 + 10);
-        }
+        double num = Math.sqrt(20);
 
     }
 }
