6faba0e82b91bccbcf7159785eb1ebaca795e14a
William Kurzius <bill@kurzius.net>
2025-10-07 08:04:57 -0400

    strings

--- helloWorld.java
+++ helloWorld.java
@@ -3,7 +3,15 @@ public class helloWorld {
 
     public static void main(String[] args) {
 
-        double num = Math.sqrt(20);
+        String s1 = "a string";
+        String s2 = "a string";
+        String s3 = "string";
+
+        boolean s12 = s1.equals(s2); // true
+        boolean s13 = s1.equals(s3); // false
+
+        System.out.println(s12);
+        System.out.println(s13);
 
     }
 }
