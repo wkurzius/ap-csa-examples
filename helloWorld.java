41cb52e17beda5a562f71b3a2128468de4dbe346
William Kurzius <bill@kurzius.net>
2025-11-10 08:08:39 -0500

    something

--- helloWorld.java
+++ helloWorld.java
@@ -3,7 +3,13 @@ public class helloWorld {
 
     public static void main(String[] args) {
 
-        
+        int i = 1;
+
+        while (i < 10) {
+            System.out.println(i);
+            i++;
+        } 
+
 
     }
 
