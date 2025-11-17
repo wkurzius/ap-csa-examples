48db286cf4f0b52221250a59ac9050bc0279447a
William Kurzius <bill@kurzius.net>
2025-11-16 21:39:12 -0500

    algos

---
+++ TwoNineAlgos.java
@@ -0,0 +1,53 @@
+public class TwoNineAlgos {
+
+    public static boolean isDivisible(int n, int d) {
+        return (n % d == 0);
+    }
+
+    public static String printDigits(int n) {
+
+        String s = "";
+        int temp = n;
+
+        while (temp != 0) {
+            s = s + (temp % 10) + " ";
+            temp /= 10;
+        }
+
+        return s;
+    }
+
+    public static int minRand(int num) {
+
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < num; i++) {
+            int rand = (int) (Math.random() * Integer.MAX_VALUE);
+            // System.out.println(rand);
+            if (rand < min)
+                min = rand;
+        }
+
+        return min;
+    }
+
+    // exclusive
+    public static int sum(int start, int end, int count) {
+
+        int sum = 0;
+
+        for (int i = start; i < end; i += count) {
+            sum += i;
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isDivisible(5, 2));
+        System.out.println(printDigits(42069));
+        System.out.println(sum(1, 10, 1));
+        System.out.println(minRand(100000000));
+    }
+
+}
